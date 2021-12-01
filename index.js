f22f5f65b5026543584447dbb2d9c9964d3a18fc
Jhonny Flores <88248110+JhonnyDark10@users.noreply.github.com>
2021-12-01 16:35:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: "#app",
+  
+    data() {
+      return {
+        courses: [],
+        title: "",
+        time: 0,
+      };
+    },
+  
+    computed: {
+      totalTime() {
+        let timeT = 0;
+        this.courses.forEach((course) => {
+          timeT += parseInt(course.time);
+        });
+        return timeT;
+      },
+    },
+  
+    methods: {
+      addCourse() {
+        this.courses.push({ title: this.title, time: this.time });
+      },
+    },
+  });
+  
